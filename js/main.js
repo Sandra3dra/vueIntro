07924ae5dd229cbf204eed3b2e6035b943c13d14
Sandra Tsao <sandratsao@Sandras-MBP.fanshawec.ca>
2020-01-14 16:54:42 -0500

    navbar

--- js/main.js
+++ js/main.js
@@ -1,21 +1,41 @@
-import Vue from 'https://cdn.jsdelivr.net/npm/vue@2.6.11/dist/vue.esm.browser.js'
+// todo => use a key to track the current video, or just pass the video in as a ref to the function and grab its source
 
-const myVM = (() => {
-    //instantiate a new Vue instance
-    let vue_vm = new Vue({
-        data: {
-            message: "Hello from Vue!",
+var vm = new Vue({
+  el: "#app",
 
-            collection: [
-                {name: "Sundra", role: "stuudenttt"},
-                {name: "Sundra lamba 2", role: "pilot"},
-                {name: "Sundra lamba 3", role: "jumping cooler"}
-            ]
-        },
-        methods: {
-            logClicked() {
-                console.log("clicked on an element");
-            }
-        }
-    }).$mount("#app");
-})();
+  data: {
+
+    // mock up the user - this well eventually come from the database UMS (user management system)
+    user: {
+        isAdmin: true,
+        isLoggedIn: true,
+        avatar: "./images/thor.png"
+    }, //important comma
+
+    // this data would also come from the database, but we'll just mock it up for now
+    videodata: [
+      { name: "Star Wars The Force Awakens", thumb: "forceawakens.jpg", vidsource: "forceawakens.mp4", description: "yet another star wars movie" },
+      { name: "Stranger Things", thumb: "strangerthings.jpg", vidsource: "strangerthings.mp4", description: "don't get lost in the upside down" },
+      { name: "Marvel's The Avengers", thumb: "avengers.jpg", vidsource: "avengers.mp4", description: "will they make black widow action figures this time?" }
+    ],
+
+    showDetails: false
+  },
+
+  methods: {
+    logInOut() {
+        //test the login / logout UI : button should cancel color
+        // eventually we will use routing and a login component
+        console.log("do login/logout on click");
+
+        // ? : is a ternary statement (shorthand for if / else) 
+        // evaluate the expression; is it's true, use the value to the 
+        // left of the colon. if it's false, use the value on the right
+        this.user.isLoggedIn = (this.user.isLoggedIn) ? false : true;
+    },
+
+    setUserPrefs() {
+        console.log("set user prefs via routing and probably a component")
+    }
+  }
+});
